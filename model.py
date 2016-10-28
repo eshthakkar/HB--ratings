b732e4aab04e9b1f210e8703dcf533c0ffca5a65
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-27 18:01:28 -0700

    Completed part of ratings predictive algorithm

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models and database functions for Ratings project."""
 
 from flask_sqlalchemy import SQLAlchemy
+import correlation
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -27,6 +28,27 @@ class User(db.Model):
         """ Provide helpful representation of user when printed"""
 
         return "<User user_id=%s email=%s>" % (self.user_id, self.email)
+
+    def similarity(self, other):
+        """Return pearson rating for user1 compared to user2."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0    
+
 
 
 class Rating(db.Model):
